20aa49b97803e38bb2f0519693214b3c197baf4f
Valentina Adami <valeadami@gmail.com>
2019-04-10 11:56:10 +0200

    commit 1

---
+++ index.js
@@ -0,0 +1,291 @@
+//10/04/2019 PROGETTO FUTURA
+/****************************************** */
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require('express-session');
+/*********** */
+const request = require('request');
+
+const querystring = require('querystring');
+const parseurl = require('parseurl');
+const path = require("path");
+const https = require('https');
+
+/*** DIALOGFLOW FULFILLMENT */
+const {WebhookClient} = require('dialogflow-fulfillment');
+
+
+/** utilità */
+const fs = require("fs");
+const utf8=require('utf8');
+//file di configurazione
+//const env = require('node-env-file');
+//env(__dirname + '/.env');
+
+
+
+
+var app = express();
+var bot='ChitChat'; // CHICKCHAT
+/*app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");*/
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+
+//inizializzo la sessione
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false, maxAge: 180000,name:'JSESSIONID'}
+  }));
+//uso le variabili di sessione
+app.use(function (req, res, next) {
+    
+    req.session.username='';
+    req.session.matId='';
+    req.session.stuId='';
+    
+  
+    next();
+  })
+  postData = querystring.stringify({
+    'searchText': 'ciao',
+    'user':'',
+    'pwd':'',
+    'ava':'FarmaInfoBot'
+    
+  });
+  //questo diventerà un modulo con la conessione a PLQ
+   const options = {
+     //modifica del 12/11/2018 : cambiato porta per supportare HTTPS
+     
+    hostname: '86.107.98.69', 
+    port: 8443,
+    rejectUnauthorized: false, 
+    path: '/AVA/rest/searchService/search_2?searchText=', 
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json', 
+      'Cookie':'' // +avaSession 
+    }
+  };
+
+
+    //PER TEST
+    app.get('/testSessione', function(req, res, next) {
+      
+          res.setHeader('Content-Type', 'text/html')
+          res.write("sono nella root ");
+          res.write('<p>views: ' + req.session.views + '</p>')
+          res.write('<p> id sessione ' + req.session.id  +' expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+        
+          res.end()
+      
+      })
+
+//CHICKCHAT
+ function WebhookProcessing(req, res) {
+    const agent = new WebhookClient({request: req, response: res});
+    //10/01/2019
+    //copiato codice da progetto api
+    console.log('------sono su FUTURA app ----- la richiesta proviene da '+ agent.requestSource);
+    var name=req.body.queryResult.intent.name;
+    //QUALSIASI INTENT RISPONDE A CALLAVA ANCHE FALLBACK
+    var displayname=req.body.queryResult.intent.displayName;
+    console.log('nome intent '+name+ ' , display name '+ displayname);
+    //******************************************* */
+  
+    //recupero la sessionId della conversazione
+    
+    agent.sessionId=req.body.session.split('/').pop();
+  //assegno all'agente il parametro di ricerca da invare sotto forma di searchText a Panloquacity
+  /*  agent.parameters['Command']=req.body.queryResult.parameters.Command;
+    if (req.body.queryResult.parameters.esame){
+
+      console.log(' ho esame =' + req.body.queryResult.parameters.esame);
+      agent.parameters['esame']=req.body.queryResult.parameters.esame;
+    }*/
+    //fulfillment text
+    agent.fulfillmentText=req.body.queryResult.fulfillmentText;
+    console.log('----> fulfillment text =' +agent.fulfillmentText);
+    console.info(` sessione agente ` + agent.sessionId +` con parametri` + agent.parameters.Command);
+  //20/03/2019 fallback su plq
+    if (req.body.queryResult.parameters.searchText){
+
+      console.log(' ho param searchText per PLQ =' + req.body.queryResult.parameters.searchText);
+      agent.parameters['searchText']=req.body.queryResult.parameters.searchText;
+    }
+    //gestione degli intent
+    //nuovo del 21/03/2019 fallback intent
+      var blnIsFallback=req.body.queryResult.intent.isFallback;
+      console.log('blnIsFallback ?? '+blnIsFallback);
+     
+     //la funzione callAva sostiutisce la funzione welcome 
+     // callAVA anytext AnyText sostituisce 'qualunquetesto'
+      let intentMap = new Map();
+      intentMap.set(displayname, callAVA);
+
+      /*if (blnIsFallback){
+  
+        //recupero il query text del body
+        var stringa=req.body.queryResult.queryText;
+        console.log('query text del fallback :'+stringa);
+        agent.queryText=stringa;
+        intentMap.set(displayname, callAVA);
+        console.log('funzione callAva per default fallback');
+      } else{
+        intentMap.set(displayname, callAVANEW); 
+        console.log('funzione callAVANEW per tutto il resto');
+      }*/
+    agent.handleRequest(intentMap);
+  }
+  
+  //app.post('/fulfillment', appDFActions);
+  app.post("/fulfillment", function (req,res){
+
+    console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
+    console.log('DIALOGFLOW Request body: ' + JSON.stringify(req.body));
+    //console.log('vedo le var di sessione di Express ?? '+ req.session.id );
+    
+    WebhookProcessing(req, res); 
+  
+  
+  });
+ 
+
+ 
+  /**** FUNZIONI A SUPPORTO copiate da progetto api */
+
+function scriviSessione(path, strSessione, strValore) {
+  
+  fs.appendFile(path + strSessione,strValore, function (err) {
+    if (err) {
+      
+      throw err;
+    
+    } else {
+    console.log('DENTRO SCRIVI SESSIONE: SALVATO FILE '+ path + strSessione);
+    
+    }
+     
+  });
+ 
+} 
+
+function leggiSessione(path, strSessione){
+  var contents='';
+  try {
+    fs.accessSync(__dirname+ '/sessions/'+ strSessione);
+    contents = fs.readFileSync(__dirname+'/sessions/'+ strSessione, 'utf8');
+    console.log('DENTRO LEGGI SESSIIONE ' +contents);
+  
+
+  }catch (err) {
+    if (err.code==='ENOENT')
+    console.log('DENTRO LEGGI SESSIONE :il file non esiste...')
+   
+  }
+  return contents;
+
+} 
+ // 18/12/2018
+ function getComandi(arComandi)
+  {
+
+    var comandi=arComandi;
+    if (comandi.length>0){
+        //prosegui con il parsing
+        //caso 1: ho solo un comando, ad esempio lo stop->prosegui con il parsing
+        switch (comandi.length){
+          case 1:
+            comandi=arComandi;
+            break;
+
+          case 2:
+          //caso 2: ho due comandi, stop e img=path image, quindi devo scomporre comandi[1] 
+            var temp=arComandi[1].toString();
+            //temp=img=https.....
+            //splitto temp in un array con due elementi divisi da uguale
+            temp=temp.split("=");
+            console.log('valore di temp[1]= ' +temp[1]);
+            arComandi[1]=temp[1];
+            comandi=arComandi;
+
+            //scompongo arComandi[1]
+            break;
+
+          default:
+            //
+            console.log('sono in default');
+
+        }
+       return comandi; //ritorno array come mi serve STOP oppure STOP, PATH img
+      
+    } else {
+      console.log('non ci sono comandi')
+
+      //non ci sono comandi quindi non fare nulla
+      return undefined;
+    }
+   
+  } 
+
+ 
+ function callAVA(agent) {
+  return new Promise((resolve, reject) => {
+ 
+  let strRicerca=agent.queryText;
+   console.log('valore di strRicerca ' + strRicerca);
+  
+  var str= utf8.encode(strRicerca); 
+  if (str) {
+    strRicerca=querystring.escape(str); 
+    options.path+=strRicerca+'&user=&pwd=&ava='+bot;
+  }
+ 
+   let data = '';
+    let strOutput='';
+ 
+    const req = https.request(options, (res) => {
+    console.log(`STATUS DELLA RISPOSTA: ${res.statusCode}`);
+    console.log(`HEADERS DELLA RISPOSTA: ${JSON.stringify(res.headers)}`);
+
+
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+     console.log(`BODY: ${chunk}`);
+     data += chunk;
+  
+     let c=JSON.parse(data);
+      strOutput=c.output[0].output;
+      strOutput=strOutput.replace(/(<\/p>|<p>|<b>|<\/b>|<br>|<\/br>|<strong>|<\/strong>|<div>|<\/div>|<ul>|<li>|<\/ul>|<\/li>|&nbsp;|)/gi, '');
+      agent.add(strOutput); 
+      resolve(agent);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.');
+      options.path='/AVA/rest/searchService/search_2?searchText=';      
+      console.log('valore di options.path FINE ' +  options.path);
+ 
+    });
+  });
+   req.on('error', (e) => {
+   console.error(`problem with request: ${e.message}`);
+   strOutput="si è verificato errore " + e.message;
+
+  });
+
+   req.write(postData);
+  req.end();
+  });
+ }
+ /* fine modifica del 21/03/2019 */
+
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("App started on port " + process.env.PORT );
+  });
+
