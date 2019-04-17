ac5be8eb2aeae23a1f9e9d2fa66fbf29c2615b11
Valentina Adami <valeadami@gmail.com>
2019-04-17 12:24:31 +0200

    revisione getComandi

--- Classi/clsPanloquacity.js
+++ Classi/clsPanloquacity.js
@@ -35,6 +35,8 @@ var responseFromPlq={
     'cmd':[]
   }
  
+  //modifica del 17/04/2019
+  /*
 function getComandi(arComandi)
   {
 
@@ -75,6 +77,63 @@ function getComandi(arComandi)
     }
    
 } 
+*/
+//aggiungo getComandi come da progetto Alexa, in modo da gestire il comando con STOP/GETLibretto/IMG=
+//["IMG=https://www.ideegreen.it/wp-content/uploads/2018/03/paguro-bernardo-3.jpg"]
+function getComandi(arComandi)
+ {
+
+   var comandi=arComandi;
+   var temp;
+   if (comandi.length>0){
+       //prosegui con il parsing
+       //caso 1: ho solo un comando, ad esempio lo stop->prosegui con il parsing
+       switch (comandi.length){
+         case 1:
+         //07/01/2019: ora il comando può contenere immagine, quindi verifica se presente =
+           
+           //comandi=arComandi;
+           temp=comandi[0].toString();
+           //è una stringa? Se si contiene il carattere "="
+           var pos = temp.indexOf("=");
+           if (pos >- 1) {
+
+            //ho una stringa, quindi splitto per "="
+            temp=temp.split("=");
+            console.log('valore di temp[1]= ' +temp[1]);
+            arComandi[0]=temp[1];
+            comandi=arComandi;
+           }
+           break;
+
+         case 2:
+         //caso 2: ho due comandi, stop e img=path image, quindi devo scomporre comandi[1] 
+            temp=arComandi[1].toString();
+           //temp=img=https.....
+           //splitto temp in un array con due elementi divisi da uguale
+           temp=temp.split("=");
+           console.log('valore di temp[1]= ' +temp[1]);
+           arComandi[1]=temp[1];
+           comandi=arComandi;
+
+           //scompongo arComandi[1]
+           break;
+
+         default:
+           //
+           console.log('sono in default');
+
+       }
+      return comandi; //ritorno array come mi serve STOP oppure STOP, PATH img
+     
+   } else {
+     console.log('non ci sono comandi')
+
+     //non ci sono comandi quindi non fare nulla
+     return undefined;
+   }
+  
+ }
 
 postData = querystring.stringify({
   'searchText': 'ciao',
