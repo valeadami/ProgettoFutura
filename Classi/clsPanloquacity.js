8d59633e00bbb56dd3e60364a585276b08ff3e29
Valentina Adami <valeadami@gmail.com>
2019-07-05 14:57:22 +0200

    replace con uno

--- Classi/clsPanloquacity.js
+++ Classi/clsPanloquacity.js
@@ -1281,7 +1281,7 @@ function callAVA(agent) {
                 //modificato il 02/07/2019 dopo richiesta a M. Salata di inserire nuovi appelli
                 //**********  modifica del 05/07/2019 su richiesta di Sergio: solo le date se c'è un solo appello ****************
                 //test di replace della stringa DIRITO PRIVATO I IN 1
-                //prenotazioni[i].adDes=prenotazioni[i].adDes.replace(/( I)/gi, " 1");
+                prenotazioni[i].adDes=prenotazioni[i].adDes.replace(/( I)/gi, " uno");
                 strTemp+= prenotazioni[i].adDes+ ' in data 8 luglio 2019, 16 luglio 2019, 7 agosto 2019.'; //Quale data vuoi scegliere?
                 
                 }
